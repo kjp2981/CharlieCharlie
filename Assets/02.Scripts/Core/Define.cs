528434ad15ec0c8ea1e3466ad38a5b0801079c26
강대희 <daehee719@gmail.com>
2022-07-11 17:28:17 +0900

    다시 텍스트 효과 연출

--- Assets/02.Scripts/Core/Define.cs
+++ Assets/02.Scripts/Core/Define.cs
@@ -1,8 +1,21 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Define
 {
-    
+    private static CinemachineVirtualCamera vCam = null;
+
+    public static CinemachineVirtualCamera VCam
+    {
+        get
+        {
+            if(vCam == null)
+            {
+                vCam = GameObject.FindWithTag("VCam").GetComponent<CinemachineVirtualCamera>();
+            }
+            return vCam;
+        }
+    }
 }
